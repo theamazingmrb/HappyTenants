ba469e971b11597065689096e2fcd817cc154ac6
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2021-03-18 14:44:46 -0700

    small route change

--- app/models/ticket.model.js
+++ app/models/ticket.model.js
@@ -1,37 +1,39 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 
 const Ticket = mongoose.model(
-  "Ticket",
+  'Ticket',
   new mongoose.Schema({
-      building: [
-          {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "Building"
-            }
-        ],
-        createdBy: [
-          {
-              type: mongoose.Schema.Types.ObjectId,
-              ref: "User"
-            }
-        ],
-      description: String,
-      image: String,
-      maintenance: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User"
-       }
-      ],
-      maintenanceNotes: [],
-      tenant: [
-        {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: "User"
-        }
-      ],
-      tenantNotes: []
-    })
-  );
+    building: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Building',
+      },
+    ],
+    createdBy: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    description: String,
+    image: String,
+    maintenance: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    maintenanceNotes: [],
+    status: String,
+    tenant: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+    tenantNotes: [],
+    unit: String,
+  })
+);
 
 module.exports = Ticket;
