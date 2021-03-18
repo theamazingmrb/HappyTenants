4bb00c4f9a3f8a0061b26cfde8d3d725a67bf097
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2021-03-18 14:51:21 -0700

    routes updates

--- app/routes/building.routes.js
+++ app/routes/building.routes.js
@@ -1,28 +1,39 @@
-const { authJwt } = require("../middlewares");
-const controller = require("../controllers/building.controller");
+const { authJwt } = require('../middlewares');
+const controller = require('../controllers/building.controller');
 
-module.exports = function(app) {
-  app.use(function(req, res, next) {
+module.exports = function (app) {
+  app.use(function (req, res, next) {
     res.header(
-      "Access-Control-Allow-Headers",
-      "x-access-token, Origin, Content-Type, Accept"
+      'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept'
     );
     next();
   });
 
   // GET
   // all
-  app.get("/api/buildings", controller.getAll);
+  app.get('/api/building/all', controller.getAll);
   // one
-  app.get("/api/building", controller.getBuilding);
+  app.get('/api/building', controller.getBuilding);
 
   // Create
-  app.post("/api/building", [authJwt.verifyToken, authJwt.isManager], controller.create);
+  app.post(
+    '/api/building',
+    [authJwt.verifyToken, authJwt.isManager],
+    controller.create
+  );
 
   // Update
-  app.put("/api/building", [authJwt.verifyToken, authJwt.isManager], controller.update);
-  
+  app.put(
+    '/api/building',
+    [authJwt.verifyToken, authJwt.isManager],
+    controller.update
+  );
+
   // Deletes
-  app.delete("/api/building/", [authJwt.verifyToken, authJwt.isManager], controller.delete);
-  
+  app.delete(
+    '/api/building/',
+    [authJwt.verifyToken, authJwt.isManager],
+    controller.delete
+  );
 };
