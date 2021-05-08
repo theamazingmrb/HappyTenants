10111cb991a1b1368b9cc8590fdccb1e7be0a741
Billie Heidelberg <billie.heidelberg.jr@gmail.com>
2021-05-08 16:44:17 -0700

    updating user creation to process building

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,17 +1,18 @@
-const config = require("../config/auth.config");
-const db = require("../models");
+const config = require('../config/auth.config');
+const db = require('../models');
 const User = db.user;
 const Role = db.role;
 const Building = db.building;
 
-var jwt = require("jsonwebtoken");
-var bcrypt = require("bcryptjs");
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcryptjs');
 
 exports.signup = (req, res) => {
   const user = new User({
     username: req.body.username,
     email: req.body.email,
-    password: bcrypt.hashSync(req.body.password, 8)
+    password: bcrypt.hashSync(req.body.password, 8),
+    building: res.body.building,
   });
 
   user.save((err, user) => {
@@ -19,22 +20,25 @@ exports.signup = (req, res) => {
       res.status(500).send({ message: err });
       return;
     }
-    if (req.body.roles.includes("tenant")){
-      Building.findByIdAndUpdate({_id: req.body.building}).then(building => {
-        building.tenants.addToSet(user._id)
-        building.save( err=> {
-          if(err) {
-            res.status(500).send({
-            message: err })
+    if (req.body.roles.includes('tenant')) {
+      Building.findByIdAndUpdate({ _id: req.body.building }).then(
+        (building) => {
+          building.tenants.addToSet(user._id);
+          building.save((err) => {
+            if (err) {
+              res.status(500).send({
+                message: err,
+              });
+            }
+          });
         }
-        })
-      })
+      );
     }
 
     if (req.body.roles) {
       Role.find(
         {
-          name: { $in: req.body.roles }
+          name: { $in: req.body.roles },
         },
         (err, roles) => {
           if (err) {
@@ -42,32 +46,32 @@ exports.signup = (req, res) => {
             return;
           }
 
-          user.roles = roles.map(role => role._id);
-          user.save(err => {
+          user.roles = roles.map((role) => role._id);
+          user.save((err) => {
             if (err) {
               res.status(500).send({ message: err });
               return;
             }
 
-            res.send({ message: "User was registered successfully!" });
+            res.send({ message: 'User was registered successfully!' });
           });
         }
       );
     } else {
-      Role.findOne({ name: "user" }, (err, role) => {
+      Role.findOne({ name: 'user' }, (err, role) => {
         if (err) {
           res.status(500).send({ message: err });
           return;
         }
 
         user.roles = [role._id];
-        user.save(err => {
+        user.save((err) => {
           if (err) {
             res.status(500).send({ message: err });
             return;
           }
 
-          res.send({ message: "User was registered successfully!" });
+          res.send({ message: 'User was registered successfully!' });
         });
       });
     }
@@ -76,9 +80,9 @@ exports.signup = (req, res) => {
 
 exports.signin = (req, res) => {
   User.findOne({
-    username: req.body.username
+    username: req.body.username,
   })
-    .populate("roles", "-__v")
+    .populate('roles', '-__v')
     .exec((err, user) => {
       if (err) {
         res.status(500).send({ message: err });
@@ -86,7 +90,7 @@ exports.signin = (req, res) => {
       }
 
       if (!user) {
-        return res.status(404).send({ message: "User Not found." });
+        return res.status(404).send({ message: 'User Not found.' });
       }
 
       var passwordIsValid = bcrypt.compareSync(
@@ -97,26 +101,26 @@ exports.signin = (req, res) => {
       if (!passwordIsValid) {
         return res.status(401).send({
           accessToken: null,
-          message: "Invalid Password!"
+          message: 'Invalid Password!',
         });
       }
 
       var token = jwt.sign({ id: user.id }, config.secret, {
-        expiresIn: 86400 // 24 hours
+        expiresIn: 86400, // 24 hours
       });
 
       var authorities = [];
 
       for (let i = 0; i < user.roles.length; i++) {
-        authorities.push("ROLE_" + user.roles[i].name.toUpperCase());
+        authorities.push('ROLE_' + user.roles[i].name.toUpperCase());
       }
-      console.log("signed in")
+      console.log('signed in');
       res.status(200).send({
         id: user._id,
         username: user.username,
         email: user.email,
         roles: authorities,
-        accessToken: token
+        accessToken: token,
       });
     });
 };
